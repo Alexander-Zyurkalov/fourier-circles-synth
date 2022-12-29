ea6abafc46def518105ead67f8f0be77f47180fa
Alexandr Zyurkalov <alexandr.zyurkalov@idealo.de>
2022-12-29 21:33:48 +0400

    adds a test for jmap, just to understand what is jmap

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,7 +1,13 @@
-
 #include <catch2/catch_test_macros.hpp>
+#include <juce_core/juce_core.h>
 
 TEST_CASE("Learning how jmap is working") {
-    REQUIRE(1 == 1);
+    float phase = juce::MathConstants<float>::pi/2;
+    float degree = juce::jmap<float>(
+            phase,
+            0.0f, juce::MathConstants<float>::pi,
+            0.0f, 180.0f
+    );
+    REQUIRE(degree == 90.0f);
 
 }
