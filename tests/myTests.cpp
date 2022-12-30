fd8b2538bbff90efdf02be49540d149a56ecdbe0
Alexandr Zyurkalov <alexandr.zyurkalov@idealo.de>
2022-12-30 22:27:20 +0400

    understanding how roundToInt works

--- tests/myTests.cpp
+++ tests/myTests.cpp
@@ -9,5 +9,10 @@ TEST_CASE("Learning how jmap is working") {
             0.0f, 180.0f
     );
     REQUIRE(degree == 90.0f);
+}
 
+TEST_CASE("Learning roundToInt"){
+    float myFloat = 3.9;
+    int myInt = juce::roundToInt(myFloat);
+    REQUIRE(myInt == 4);
 }
