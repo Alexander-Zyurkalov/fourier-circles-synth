e71cb7a954c4a0d1f9b068fe8472fd4261dc9732
Alexandr Zyurkalov <alexandr.zyurkalov@idealo.de>
2023-02-02 20:37:13 +0100

    replaces with another function

--- scripts/signalgen.py
+++ scripts/signalgen.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import ndarray
+from scipy import signal
 
 
 def sine_wave(f: float, over_sample_rate: float, phase: float, n_cyl: int) -> tuple[ndarray, ndarray]:
@@ -12,5 +13,5 @@ def sine_wave(f: float, over_sample_rate: float, phase: float, n_cyl: int) -> tu
 def square_wave(f: float, over_sample_rate, rate: float, n_cyl: int) -> tuple[ndarray, ndarray]:
     fs = over_sample_rate * f
     t = np.arange(0, n_cyl * 1 / f - 1 / fs, 1 / fs)
-    g = np.sign(np.sin(2 * np.pi * f * t))
+    g = signal.square(2 * np.pi * f * t, duty=0.5)
     return t, g
