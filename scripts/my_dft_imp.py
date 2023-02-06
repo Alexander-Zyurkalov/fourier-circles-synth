bbc9cbbab7f209d22d89aeae0b545a279357974a
Alexandr Zyurkalov <alexandr.zyurkalov@idealo.de>
2023-02-06 20:34:14 +0100

    reformatting

--- scripts/my_dft_imp.py
+++ scripts/my_dft_imp.py
@@ -10,12 +10,12 @@ def my_dft_imp(xns: ndarray[float]) -> tuple[ndarray, ndarray]:
     for k in range(0, int(N / 2)):
         Xre[int(k)] = \
             2 / N * \
-                np.sum(
-                     np.full(N,
+            np.sum(
+                np.full(N,
                         fill_value=[
                             xns[int(n)] * np.cos(k * n * np.pi * 2 / N) for n in range(0, N)
                         ])
-                )
+            )
 
     index = np.arange(0, int(N / 2), 1)
     return index, Xre
